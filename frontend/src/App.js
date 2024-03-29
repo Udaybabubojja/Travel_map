22cc2523a92826176bbcd1c22b3247011e99c673
Udaybabubojja <21pa1a0518@vishnu.edu.in>
2024-03-29 11:37:43 +0530

    Front end is updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import * as React from 'react';
+import { useState } from 'react';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map, {Marker, Popup} from 'react-map-gl';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import StarIcon from '@mui/icons-material/Star';
+import "./App.css"
 function App() {
+  const [showPopup, setShowPopup] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      mapboxAccessToken={process.env.REACT_APP_TOKEN}
+      initialViewState={{
+        longitude: 48,
+        latitude: 17,
+        zoom: 4.5
+      }}
+      style={{width: "100%", height: "100vh"}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    >
+      <Marker longitude={48.5} latitude={16.6} offsetLeft={-20} offsetTop={-10} > 
+      <LocationOnIcon style={{ color: 'red', fontSize: "70px"}} />
+      {showPopup && (
+      <Popup longitude={48.5} latitude={16.6}
+        anchor="bottom"
+        onClose={() => setShowPopup(false)}>
+        <div className='Card'>
+          <label>Place: </label>
+          <h4 className='place'>Dubai</h4>
+          <label>Rating: </label>
+          <div className='stars'>
+            <StarIcon/>
+            <StarIcon/>
+          </div>
+          <label>Review: </label>
+          <p className='review'>This is a Beautiful place with lot of peace and neatness..</p>
+          <label>Information: </label>
+          <div className='user-details'>
+            <span className='username'>Created By <b>Uday</b></span> <br/>
+            <span className='date'>Date: <b>23-03-2023</b></span>
+          </div>
+        </div>
+      </Popup>)}
+    </Marker>
+  </Map>
   );
 }
 
